5f068c9dce8bc94d22742ea557189b7055d50f2c
nfeuer <albenk234@gmail.com>
2017-09-16 23:36:48 -0400

    fixed a firebase deploy issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import{AngularFireModule} from "angularfire2";
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
-import {environment} from "../environments/environment";
+import {environment} from "../environments/environment.prod";
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBwgv-XOya5d6Hti6oTaglm4I-_q0HN1Q0",
+    authDomain: "eshop-6e981.firebaseapp.com",
+    databaseURL: "https://eshop-6e981.firebaseio.com",
+    projectId: "eshop-6e981",
+    storageBucket: "eshop-6e981.appspot.com",
+    messagingSenderId: "482608271783"
+  }
 };
