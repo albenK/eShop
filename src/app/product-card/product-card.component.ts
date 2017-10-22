fdcffdc28cef32a14baa451f72848275efbe878a
nfeuer <albenk234@gmail.com>
2017-10-21 23:17:09 -0400

    Added quantity update and some minor css changes.

--- src/app/product-card/product-card.component.ts
+++ src/app/product-card/product-card.component.ts
@@ -21,8 +21,8 @@ export class ProductCardComponent implements OnInit {
   addToShoppingCart(product:Product) {
     this.shoppingCartService.addToShoppingCart(product);
   }
-
-  getQuantityOfItemInShoppingCart():number {
+  // for displaying the quantity of items in shopping cart.
+  getQuantity():number {
     if(!this.shoppingCart || !this.shoppingCart.Items) return 0;
     let item = this.shoppingCart.Items[this.product.$key];
     return (item)?(item.quantity):(0);
