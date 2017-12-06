c68ac8eca257569766e400952be07b48aa0f35e1
nfeuer <albenk234@gmail.com>
2017-12-05 23:29:11 -0500

    Added SharedModule to help modularize the app. Still need to continue working on this.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,53 +1,35 @@
-//AngularFire imports.
-import{AngularFireModule} from "angularfire2";
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
-import {environment} from "../environments/environment.prod";
-import {CustomFormsModule} from "ng2-validation";
+import { CustomFormsModule } from 'ng2-validation';
 
-//Angular imports needed to setup app module.
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule} from '@angular/core';
-import {FormsModule} from "@angular/forms";
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {RouterModule,Routes} from "@angular/router";
-
-//import services used by our app.
-import {AuthService} from "./shared/services/auth.service";
-import {UserService} from "./shared/services/user.service";
-import {CategoryService} from "./shared/services/category.service";
-import {ProductService} from "./shared/services/product.service";
-import {ShoppingCartService} from "./shared/services/shopping-cart.service";
-import { OrderService } from "./shared/services/order.service";
-
-//import Route Guards used by our app.
-import {AuthGuardService} from "./shared/services/auth-guard.service";
-import {AdminAuthGuardService} from "./admin-auth-guard.service";
-
-//import custom AngularMaterialModule
-import {AngularMaterialModule} from "./angular-material/angular-material.module";
-
-//import all components used by our app.
-import { AppComponent } from './app.component';
-import { AppToolbarComponent } from './app-toolbar/app-toolbar.component';
-import { HomeComponent } from './home/home.component';
-import { ProductsComponent } from './products/products.component';
-import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
-import { CheckOutComponent } from './check-out/check-out.component';
-import { OrderSuccessComponent } from './order-success/order-success.component';
-import { MyOrdersComponent } from './my-orders/my-orders.component';
-import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { environment } from '../environments/environment.prod';
+import { AdminAuthGuardService } from './admin-auth-guard.service';
 import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
-import { LoginComponent } from './login/login.component';
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
 import { ProductFormComponent } from './admin/product-form/product-form.component';
+import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { AppToolbarComponent } from './app-toolbar/app-toolbar.component';
+import { AppComponent } from './app.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MyOrdersComponent } from './my-orders/my-orders.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductFilterComponent } from './products/product-filter/product-filter.component';
-import { ProductCardComponent } from './shared/components/product-card/product-card.component';
-import { ProductQuantityComponent } from './shared/components/product-quantity/product-quantity.component';
-import { ShoppingCartSummaryComponent } from './shopping-cart-summary/shopping-cart-summary.component';
+import { ProductsComponent } from './products/products.component';
+import { AuthGuardService } from './shared/services/auth-guard.service';
 import { ShippingFormComponent } from './shipping-form/shipping-form.component';
+import { ShoppingCartSummaryComponent } from './shopping-cart-summary/shopping-cart-summary.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { SharedModule } from 'shared/shared.module';
 
-//set up our routes.
 const routes:Routes = [
   {path:"",component:ProductsComponent},
   {path:"products",component:ProductsComponent},
@@ -100,31 +82,23 @@ const routes:Routes = [
     LoginComponent,
     PageNotFoundComponent,
     ProductFilterComponent,
-    ProductCardComponent,
-    ProductQuantityComponent,
     ShoppingCartSummaryComponent,
     ShippingFormComponent
   ],
   imports: [
     BrowserModule,
+    AngularMaterialModule,
+    SharedModule,
     BrowserAnimationsModule,
     FormsModule,
     CustomFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFireDatabaseModule,
-    AngularMaterialModule,
     RouterModule.forRoot(routes)
   ],
   providers: [
-    AuthService,
-    AuthGuardService,
-    AdminAuthGuardService,
-    UserService,
-    CategoryService,
-    ProductService,
-    ShoppingCartService,
-    OrderService
+    AdminAuthGuardService
   ],
   bootstrap: [AppComponent]
 })
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,43 @@
+import { AngularMaterialModule } from '../angular-material/angular-material.module';
+import { ProductService } from './services/product.service';
+import { CategoryService } from './services/category.service';
+import { ShoppingCartService } from 'shared/services/shopping-cart.service';
+import { OrderService } from 'shared/services/order.service';
+import { UserService } from './services/user.service';
+import { AuthGuardService } from './services/auth-guard.service';
+import { AuthService } from './services/auth.service';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { ProductQuantityComponent } from './components/product-quantity/product-quantity.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+/* 
+  ABOUT ME: The purpose of this module is to hold shared components
+  and services so that we can import this module into AppModule.
+  Helps with modularizing the app.
+*/
+@NgModule({
+  imports: [
+    CommonModule,
+    AngularMaterialModule,
+  ],
+  declarations: [
+    ProductCardComponent,
+    ProductQuantityComponent,
+  ],
+  providers:[
+    AuthService,
+    AuthGuardService,
+    UserService,
+    CategoryService,
+    ProductService,
+    ShoppingCartService,
+    OrderService
+  ],
+  exports:[
+    ProductCardComponent,
+    ProductQuantityComponent,
+  ],
+})
+export class SharedModule { }
